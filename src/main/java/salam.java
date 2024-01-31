614ffc41f20efbea2a9a186ce195fce58b2ea41e
sayed zekria <sayed.zekria.sayedi@tekschool.us>
2024-01-31 13:58:52 -0600

    new add

--- src/main/java/salam.java
+++ src/main/java/salam.java
@@ -3,5 +3,7 @@ public class salam {
         System.out.println("salam watandar");
         System.out.println("salam Takhar");
         System.out.println("salam Farkhar");
+        System.out.println("Codding is very fun");
+        System.out.println("Madison is the state capital of wisconsin");
     }
 }
